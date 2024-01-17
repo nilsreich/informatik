ae9f3660e0e4e5da60d5ad080cd321f449ba6dc9
nilsreich <nilsreich@gmail.com>
2024-01-17 11:10:12 +0000

    Update README.md with project details and resources

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="grow ">
+      <img src="https://news.ubc.ca/wp-content/uploads/2023/08/AdobeStock_559145847-1024x682.jpeg" alt="" className="h-96 w-full object-cover" />
+
+      <div className="flex justify-between  gap-4 py-2 px-4">
+        <div className="max-w-[100ch] grow mx-auto">
+          <div className="py-8 pb-8 text-6xl">Kreativer Umgang mit Texten</div>
+<div className="border rounded inline-block border-foreground px-4 py-1  text-sm">Übungen</div>
+          <div className="pb-4 pt-12 text-3xl font-medium">Vorwort</div>
+          <div className="leading-7">In unserer schnelllebigen digitalen Landschaft, wo Informationen in Sekundenschnelle übermittelt werden und der erste Eindruck oft online stattfindet, ist die Fähigkeit, klare, prä-zise und visuell ansprechende Dokumente zu erstellen, von unschätzbarem Wert. Textverar-beitungsprogramme sind dabei weit mehr als nur Werkzeuge zur Erstellung von Texten; sie sind vielseitige Plattformen, die es ermöglichen, komplexe Ideen und Konzepte auf innovati-ve und ansprechende Weise zu kommunizieren. Der professionelle Umgang mit diesen Programmen ist in der Geschäftswelt längst zu einer Grundvoraussetzung geworden. Ein gut strukturiertes, ästhetisch ansprechendes Dokument spiegelt Professionalität wider und kann in vielen Fällen über den Erfolg einer Präsentation, eines Vorschlags oder eines Berichts entscheiden. In diesem Kontext geht es nicht nur da-rum, Informationen zu vermitteln, sondern diese auch in einer Weise zu präsentieren, die die Aufmerksamkeit des Lesers fesselt und eine klare Botschaft vermittelt. Im akademischen Bereich eröffnet der kreative Einsatz von Textverarbeitungssoftware neue Horizonte für die Darstellung von Forschungsergebnissen und theoretischen Konzepten. Die Fähigkeit, komplexe Informationen nicht nur korrekt, sondern auch visuell ansprechend zu präsentieren, ist ein entscheidender Faktor für effektive Kommunikation und Lehre. Darüber hinaus ist Kreativität in der Textverarbeitung ein Schlüssel zur Differenzierung und Personalisierung von Kommunikation. In einer Welt, in der Standardformate und -vorlagen allgegenwärtig sind, ermöglicht die kreative Anpassung von Dokumenten, sich von der Mas-se abzuheben und eine persönliche oder markenspezifische Identität zu schaffen. Die folgenden Aufgaben zielen nicht nur darauf ab die technischen Fähigkeiten die zur Nut-zung von Textverarbeitungsprogrammen erforderlich sind, anzuwenden, sondern auch zu inspirieren, diese Werkzeuge auf innovative und kreative Weise einzusetzen. Es geht darum, die eigenen Grenzen der digitalen Textgestaltung zu erweitern und die vielfältigen Möglich-keiten, die sich durch einen professionellen und kreativen Umgang mit Textverarbeitung ergeben, voll auszuschöpfen.</div>
+          <div className="pb-4 pt-4 mt-8  text-3xl font-medium border-t">Einleitung</div>
+          <div className="leading-7">Die Schülerinnen und Schüler werden dazu angeleitet, ihre vorhandenen Kenntnisse in der Textverarbeitung zu vertiefen, indem sie ein anspruchsvolles kreatives Projekt im Rahmen eines selbstgewählten Themas im Bereich digitaler Technologien umsetzen. Dieser Prozess beginnt mit der gründlichen Recherche zum gewählten Thema, wobei aktuelle Entwicklun-gen und relevante Informationen eingehend untersucht werden. Dabei kommt auch der Ein-satz von KI-Tools zur Unterstützung der Recherche zum Tragen, um eine effiziente Informa-tionsgewinnung und -verarbeitung zu ermöglichen. Nach der umfassenden Recherche folgt das Verfassen eines informativen Textes für den faltbaren Flyer. Hierbei wird den Schülerinnen und Schülern die Nutzung von KI-Tools er-laubt, um die Qualität und Präzision des erstellten Inhalts zu optimieren. Dieser Schritt be-tont nicht nur die textverarbeitenden Fertigkeiten, sondern auch die Integration moderner Technologien in den kreativen Schaffensprozess. Die Arbeitsstruktur beinhaltet neben der Recherche und einer Brainstorming-Map auch die Gestaltung eines modernen Flyers im Querformat mit Vorder- und Rückseite. Die Schülerin-nen und Schüler werden dazu angeleitet, ein ansprechendes Design zu entwickeln, welches nicht nur ästhetisch überzeugt, sondern auch mindestens ein aussagekräftiges Bild sowie einen Abschnitt mit Quellen und Links zu weiterführenden Informationen beinhaltet. Parallel dazu erfolgt die Erstellung einer Präsentation, die das gewählte Thema umfassend darstellt und das gleiche Design und Farbschema des Flyers weiterführt. Dieser Schritt för-dert nicht nur die Fähigkeiten in der visuellen Präsentation, sondern betont auch die Kohä-renz zwischen den verschiedenen Elementen des Projekts. Ein weiterer wichtiger Aspekt ist die Übersetzung des Flyers in die englische Sprache. Hier lernen die Schülerinnen und Schüler, wie sie ihre Arbeit international präsentieren können, wobei darauf geachtet wird, dass das Design und die visuelle Ästhetik des Originals beibe-halten werden. Dies unterstreicht die Bedeutung von sprachlicher Vielfalt und globaler Kommunikation im Kontext digitaler Technologien.</div>
+          <div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+        <div className="w-[250px] pt-8 hidden md:block">
+          <div className="sticky top-8 border-l-2 border-neutral-800 pl-4">
+            <div className="py-1 text-sm font-medium">Inhaltsverzeichnis</div>
+            <div className="py-1 text-muted-foreground">Vorwort</div>
+            <div className="py-1 text-muted-foreground">Einleitung</div>
+            <div className="py-1 text-foreground">Themen</div>
+            <div className="py-1 text-muted-foreground">Aufgaben</div>
+            <div className="py-1 text-muted-foreground">Abgaben</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
